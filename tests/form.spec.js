a087a08a3693f67e8e102c8287e63f18c473f22e
DiNeves <dms.neves@gmail.com>
2025-11-05 18:41:26 +0000

    Improve the test structure to validate the form page section

--- tests/form.spec.js
+++ tests/form.spec.js
@@ -2,22 +2,29 @@ import { test, expect } from '@playwright/test';
 import { users } from './data/form';
 
 // Tests if the form is successfully filled for the mocked data contained in the form.js file.
-test('Form successfully filled', async ({ page }) => {
-
-for (const user of users) {
-  await page.goto('/form');
-  await page.getByRole('textbox', { name: 'Name *' }).fill(user.name);
-  await page.getByRole('textbox', { name: 'Email *' }).fill(user.email);
-  await page.getByRole('textbox', { name: 'Password *' }).fill(user.password);
-  await page.getByLabel('Country *').selectOption(user.country);
-  await page.locator('label').getByText( user.gender, {exact: true} ).click();
-
-  for (const hobbie of user.hobbies) {
-    await page.getByRole('checkbox', { name: hobbie }).check();
-  }
+test.describe('Test form page fullfilment for multiple users', () => {
   
-  await page.getByRole('button', { name: 'Send' }).click();
-  await expect(page.getByText('Success!')).toBeVisible();
-  await expect(page.getByText('The form has been submitted')).toBeVisible();
-}
+  users.forEach(user => {
+
+    test(`Fill the form page with ${user.name}'s data`, async ({ page }) => {
+  
+      await page.goto('/form');
+      await page.getByRole('textbox', { name: 'Name *' }).fill(user.name);
+      await page.getByRole('textbox', { name: 'Email *' }).fill(user.email);
+      await page.getByRole('textbox', { name: 'Password *' }).fill(user.password);
+      await page.getByLabel('Country *').selectOption(user.country);
+      await page.getByRole('radio', { name: user.gender, exact: true }).check();
+      
+      // only search for hobbies if there is, at least, 1 hobbie in the user's options
+      if (user.hobbies && user.hobbies.length > 0) {
+        for (const hobbie of user.hobbies) {
+          await page.getByRole('checkbox', { name: hobbie }).check();
+        }
+      }
+      
+      await page.getByRole('button', { name: 'Send' }).click();
+      await expect(page.getByText('Success!')).toBeVisible();
+      await expect(page.getByText('The form has been submitted')).toBeVisible();
+    })
+  })
 });
